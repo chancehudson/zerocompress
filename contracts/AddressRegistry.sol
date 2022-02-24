b40549562f7cf6fc889d12d197ff29a7121500c6
Chance Hudson <jchancehud@gmail.com>
2022-02-23 22:32:49 -0600

    refactor: registry full logic

--- contracts/AddressRegistry.sol
+++ contracts/AddressRegistry.sol
@@ -18,9 +18,8 @@ contract AddressRegistry {
     require(zeroCount < 16, 'nosav');
     // otherwise we'll store it
     require(idByAddress[a] == 0, 'dupe');
-    require(latestId < type(uint24).max, 'full');
-    id = ++latestId;
-    assert(addressById[id] == address(0));
+    id = latestId++;
+    require(addressById[id] == address(0), 'full');
     addressById[id] = a;
     idByAddress[a] = id;
   }
