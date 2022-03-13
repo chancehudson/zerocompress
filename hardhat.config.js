9d3fd129bf6eb1d18245889325418d307cbbd521
Chance Hudson <jchancehud@gmail.com>
2022-03-13 17:18:58 -0500

    chore: add arbitrum

--- hardhat.config.js
+++ hardhat.config.js
@@ -23,6 +23,13 @@ module.exports = {
         '0x18ef552014cb0717769838c7536bc1d3b1c800fe351aa2c38ac093fa4d4eb7d6',
       ],
     },
+    arbitrum: {
+      url: 'https://rinkeby.arbitrum.io/rpc',
+      accounts: [
+        // 0xeb465b6C56758a1CCff6Fa56aAee190646A597A0
+        '0x18ef552014cb0717769838c7536bc1d3b1c800fe351aa2c38ac093fa4d4eb7d6',
+      ]
+    }
   },
   mocha: {
     timeout: 300000,
