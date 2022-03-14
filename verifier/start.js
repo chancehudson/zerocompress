b4ca91dc598e75bd45498ce164023f9a98eec76b
Chance Hudson <jchancehud@gmail.com>
2022-03-13 23:26:36 -0500

    refactor: verify compression using ganache evm and historical goerli transactions

--- verifier/start.js
+++ verifier/start.js
@@ -2,38 +2,28 @@ const { ethers } = require('ethers')
 const Decompress = require('../artifacts/contracts/Decompress.sol/Decompress.json')
 const Verifier = require('../artifacts/contracts/Verifier.sol/Verifier.json')
 const { compress } = require('../src')
+const { promises: fs } = require('fs')
 
 const PRIVATE_KEY = '0x0000000000000000000000000000000000000000000000000000000000000001'
-const NODE_URL = 'ws://192.168.1.198:9546'
+// const NODE_URL = 'ws://192.168.1.198:9546'
+// const NODE_URL = 'wss://mainnet.optimism.io'
 
 async function main() {
   let verifiedCount = 0
-  // start watching for optimism transactions and try to compress
-  // and then decompress all of them
-  const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545')
-  const wallet = new ethers.Wallet(PRIVATE_KEY, provider)
-  const { verifier, decompress } = await deploy(wallet)
-  // now start watching for transactions
   const ethProvider = new ethers.providers.WebSocketProvider(NODE_URL)
-  let start = await ethProvider.getBlockNumber()
-  while (start > 0) {
-    const block = await ethProvider.getBlockWithTransactions(start)
-    for (const tx of block.transactions) {
-      if (!tx.data || tx.data.length <= 2) continue
-      // otherwise try compressing
-      const [ func, data ] = compress(tx.data)
-      const out = await verifier[func](data)
-      if (out !== tx.data) {
-        console.log('Compression mismatch!')
-        console.log(tx.hash)
-      } else {
-        verifiedCount++
-        if (verifiedCount % 100 === 0) {
-          console.log(`Verified ${verifiedCount} transactions`)
-        }
-      }
+  const end = await ethProvider.getBlockNumber()
+  for (let x = 0; x < end; x++) {
+    const block = await ethProvider.getBlockWithTransactions(x)
+    const inputTxs = block.transactions.filter((tx) => tx.data && tx.data.length > 2)
+    if (inputTxs.length === 0) {
+      await new Promise(r => setTimeout(r, 1000))
+      continue
     }
-    start--
+    const rows = inputTxs
+      .map((tx) => `${x},${tx.from},${tx.to},${tx.data}`)
+      .join('\n') + '\n'
+    await fs.appendFile('./blocks.csv', rows)
+    await new Promise(r => setTimeout(r, 1000))
   }
 }
 
