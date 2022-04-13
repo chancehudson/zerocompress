37f73dc0f7d4daf2e35f5937dbdb977c2fa06de9
Chance Hudson <jchancehud@gmail.com>
2022-04-12 23:37:34 -0500

    feat: parallel calc

--- verifier/start.js
+++ verifier/start.js
@@ -1,30 +1,38 @@
 const { ethers } = require('ethers')
 const Decompress = require('../artifacts/contracts/Decompress.sol/Decompress.json')
 const Verifier = require('../artifacts/contracts/Verifier.sol/Verifier.json')
-const { compress } = require('../src')
+const { compress, gasCost } = require('../src')
 const { promises: fs } = require('fs')
+const ParallelPromise = require('@jchancehud/parallel-promise')
 
 const PRIVATE_KEY = '0x0000000000000000000000000000000000000000000000000000000000000001'
 // const NODE_URL = 'ws://192.168.1.198:9546'
 // const NODE_URL = 'wss://mainnet.optimism.io'
+const NODE_URL = 'wss://opt-mainnet.g.alchemy.com/v2/wquJNw5twnWCHVfHU3cGyqKKkV50W9tJ'
 
 async function main() {
   let verifiedCount = 0
+  let totalGasSavings = 0
   const ethProvider = new ethers.providers.WebSocketProvider(NODE_URL)
   const end = await ethProvider.getBlockNumber()
-  for (let x = 0; x < end; x++) {
+  await ParallelPromise(end, async (x) => {
     const block = await ethProvider.getBlockWithTransactions(x)
     const inputTxs = block.transactions.filter((tx) => tx.data && tx.data.length > 2)
     if (inputTxs.length === 0) {
-      await new Promise(r => setTimeout(r, 1000))
-      continue
+      return
     }
-    const rows = inputTxs
-      .map((tx) => `${x},${tx.from},${tx.to},${tx.data}`)
-      .join('\n') + '\n'
-    await fs.appendFile('./blocks.csv', rows)
-    await new Promise(r => setTimeout(r, 1000))
-  }
+    for (const tx of inputTxs) {
+      const [func, data] = compress(tx.data)
+      const compressed = `0x${[data].flat().map(d => d.replace('0x', '')).join('')}aaaaaaaa`
+      const compressedGas = gasCost(compressed)
+      const originalGas = gasCost(tx.data)
+      if (compressedGas >= originalGas) continue
+      totalGasSavings += originalGas - compressedGas
+    }
+    console.log(x, end)
+    console.log(totalGasSavings)
+  }, limit = 40)
+  // for (let x = 0; x < end; x++) { }
 }
 
 async function deploy(wallet) {
